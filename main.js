1e66723e97827b99b9559ebbb7f306f31f9c0425
Maria SJ <maria.som.jur@gmail.com>
2022-10-31 12:55:52 +0100

    rama

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ const goblins = 2;
 const huargos = 3;
 const trolls = 5;
 
+//variables bonus
+
+let userPoints = 0
+let pcPoints = 0
+let moves = 0
+
 //functions
 
 function getRandomNumber(max) {
@@ -21,41 +27,52 @@ function avatarNumber(x){
     if (x === 1){
         return sureños;
     }
-    if (x === 2){
+    else if (x === 2){
         return orcos;
     }
-    if (x === 3){
+    else if (x === 3){
         return goblins;
     }
-    if (x === 4){
+    else if (x === 4){
         return huargos;
     }
-    if (x === 5){
+    else if (x === 5){
         return trolls;
-    }  
+    }
 };
 
 function paintText (){
     const randomNumer = getRandomNumber(5);
     const avatarRandom = avatarNumber(randomNumer);
-    const selectValue = select.value;
+    const selectValue = parseInt(select.value);
 
     if (selectValue > avatarRandom) {
         textBox.innerHTML = '¡Ha ganado el Ejército del Bien! Enhorabuena';
     }
-    if (selectValue < avatarRandom) {
+    else if (selectValue < avatarRandom) {
         textBox.innerHTML = '¡Ha ganado el Ejército del Mal! Vuelve a Intentarlo';
     }
-    if (selectValue === avatarRandom) {
+    else if (selectValue === avatarRandom) {
         textBox.innerHTML = 'Empate';
     }
-};
+}
 
 function handleClick(event) {
     event.preventDefault();
     getRandomNumber();
     paintText();
 };
+
+//BONUS:
+/*
+-calcular los puntos del usu0ario y de ordenador (contador usuario y contador ordenador)
+-contar las partidas (10 movimientos) contador de movimientos
+- cuando llegue a 10 se esconda el boton batalla y aparezca botón reinciar juego: classList
+- poner todos los contenedores a 0
+- Mostrar quién ha ganado
+*/
+
+
 
 //events
 
