c4a79004a408cb151536dbedb53e4c3d42d09260
Maria SJ <maria.som.jur@gmail.com>
2022-10-31 10:33:34 +0100

    js

--- main.js
+++ main.js
@@ -5,21 +5,48 @@ const select = document.querySelector('.js_select');
 const btn = document.querySelector('.js_btn');
 const textBox = document.querySelector('.js_text');
 
-let suereños = 2;
-let orcos = 2;
-let goblins = 2;
-let huargos = 3;
-let trolls = 5;
+const sureños = 2;
+const orcos = 2;
+const goblins = 2;
+const huargos = 3;
+const trolls = 5;
 
 //functions
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
-    }
+}
 
+
+function avatarNumber (){
+    if (sureños === getRandomNumber(1));
+
+    if (orcos === getRandomNumber(2));
     
+};
+
+function paintText (){
+    const randomNumer = getRandomNumber(5);
+    const selectValue = select.value;
+
+    if (selectValue > ) {
+        textBox.innerHTML = 'Ha ganado el Ejército del Bien! Enhorabuena';
+    }
+    if (selectValue < ) {
+        textBox.innerHTML = 'Ha ganado el Ejército del Mal! Vuelve a Intentarlo';
+    }
+    if (selectValue === ) {
+        textBox.innerHTML = 'Empate';
+    }
+};
+
+
+
 function handleClick(event) {
     event.preventDefault();
+    getRandomNumber();
+    avatarNumber();
+    paintText();
 }
 
 
